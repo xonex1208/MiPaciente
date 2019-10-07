bc420eeb26d83f7b0ac5d83bc80a8af76de68360
Cesar <32628077+xonex1208@users.noreply.github.com>
2019-10-07 10:14:36 -0500

    Agregue un boton de ejemplo

--- app/src/main/java/com/proyecto/mipaciente/activities/Citas.java
+++ app/src/main/java/com/proyecto/mipaciente/activities/Citas.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.proyecto.mipaciente.R;
 
-public class MainActivity extends AppCompatActivity {
+public class Citas extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_citas);
     }
 }
