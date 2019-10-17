6c0b7bd92d3c67b56943982bf22f6f164e9cc401
abiiy <bal-mart_@hotmail.com>
2019-10-17 18:18:46 -0500

    login
    creacion de login

---
+++ app/src/main/java/com/proyecto/mipaciente/activities/Login.java
@@ -0,0 +1,17 @@
+package com.proyecto.mipaciente.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.proyecto.mipaciente.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
