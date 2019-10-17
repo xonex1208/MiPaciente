c06da960c27088571a3e1df679a9ec5e8990f272
Cesar <32628077+xonex1208@users.noreply.github.com>
2019-10-17 13:04:48 -0500

    Estandar de botones, fuente Roboto y paleta de colores.

---
+++ app/src/main/java/com/proyecto/mipaciente/activities/RegistroPaciente.java
@@ -0,0 +1,16 @@
+package com.proyecto.mipaciente.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.proyecto.mipaciente.R;
+
+public class RegistroPaciente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_paciente);
+    }
+}
