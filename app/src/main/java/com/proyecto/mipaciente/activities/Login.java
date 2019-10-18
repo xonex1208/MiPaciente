c5d4b2750a4c4adab69eb458300a57fddf2e3894
abiiy <bal-mart_@hotmail.com>
2019-10-18 09:03:52 -0500

    cambio de nombre de variable

--- app/src/main/java/com/proyecto/mipaciente/activities/Login.java
+++ app/src/main/java/com/proyecto/mipaciente/activities/Login.java
@@ -5,16 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.proyecto.mipaciente.R;
 
 public class Login extends AppCompatActivity {
 
-    private FirebaseDatabase firebaseDatabase;
+    private FirebaseFirestore firebaseFirestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        firebaseFirestore.getClass();
 
     }
 }
